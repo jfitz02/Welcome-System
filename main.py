7575c65eb70d76a0f78bec088d740be866a9b4c2
jfitz02 <73333523+jfitz02@users.noreply.github.com>
2021-09-26 16:21:24 +0100

    included LED adjustments (From Alex)

--- main.py
+++ main.py
@@ -4,6 +4,31 @@ import os
 import numpy as np
 import pickle
 
+import pigpio # Allows pi to control the pins!
+
+pi = pigpio.pi()
+
+colours = {} # Dictionary containing preferred colours of each user
+RED_PIN = 0 # The pin numbers of the red pin
+GREEN_PIN = 0
+BLUE_PIN = 0
+
+
+with open("colours.txt") as f: # Read the users' colour preferences from the colours.txt settings file
+    lines = f.readlines() # Settings should be formatted as "name red_value green_value blue_value"
+    for line in lines:
+        name, red, green, blue = line.split(' ')
+        colours[name] = (int(red), int(green), int(blue))
+        
+
+
+def change_lights(name: str): # Change the colour of the lights to the name's preferred colour
+    colour = colours.get(name, (255, 255, 255)) # If you can't get the options for a name, then default to white
+    pi.set_PWM_dutycycle(RED_PIN, colour[0])
+    pi.set_PWM_dutycycle(GREEN_PIN, colour[1])
+    pi.set_PWM_dutycycle(BLUE_PIN, colour[2])
+
+
 
 
 with open('dataset_faces.dat', 'rb') as f:
@@ -48,3 +73,7 @@ while True:
         name = face_names[best_match_index]
 
     print(name)
+
+    change_lights(name) # Change the lights to the preferred colour of the person with given name
+
+pi.stop() # Stop the pigpio stuff when this script ends
